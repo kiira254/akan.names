effc341bd8a668531ee545fe63ba39e74b3a7cb9
nelly kamotho <nkamothO57@gmail.com>
2020-07-17 19:39:55 +0300

    add array

--- js/scripts.js
+++ js/scripts.js
@@ -7,3 +7,10 @@ function day() {
     var CC = (YY - 1) / 100 + 1;
   
     var weekDay = ( ( (CC/4) -2*CC-1) + ((5*YY/4) ) + ((26*(MM+1)/10)) + DD )%7;
+    console.log( weekDay);
+    document.getElementById("result").innerHTML = Math.round( weekDay);
+  
+    var maleNames= ["kwasi", "kwadwo", "kwabena", "kwaku", "yaw", "kofi"," kwame"];
+    var femaleNames =["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
+    var choice = document.getElementById("select").value;
+  
